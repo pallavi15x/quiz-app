976d8df9147e1a90786d4b514db33567c68f168a
pallavi15x <pallavikumari15102007@gmail.com>
2026-03-22 18:26:13 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/quiz-app/',  // ✅ must match your repo name
+  base: '/quiz-app/',   // ✅ VERY IMPORTANT
   plugins: [react()],
 })
